a95f160a5df4423c8982c01f4632f8209e29b446
Aidan Horton <Aidan.Horton@malvern.com>
2019-10-28 13:54:26 +0000

    Add UI population of board based off piece collection

--- Chess/GameManager.cs
+++ Chess/GameManager.cs
@@ -9,6 +9,15 @@
         public GameManager(MainWindow window)
         {
             this._mainWindow = window;
+
+            this.PieceManager = new PieceManager();
+
+            this.UpdateBoardUi();
+        }
+
+        private void UpdateBoardUi()
+        {
+            this._mainWindow.UpdateBoardUi(this.PieceManager.Pieces);
         }
 
         private MainWindow _mainWindow;
--- Chess/MainWindow.xaml.cs
+++ Chess/MainWindow.xaml.cs
@@ -1,15 +1,17 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.Collections.Specialized;
 using System.Windows;
 using System.Windows.Controls;
+using System.Windows.Media.Imaging;
 
 namespace Chess
 {
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class MainWindow
     {
         public ObservableCollection<LogEntry> Log = new ObservableCollection<LogEntry>();
 
@@ -25,12 +27,56 @@ namespace Chess
             this.Log.CollectionChanged += UpdateLogScroll;
         }
 
+        public void UpdateBoardUi(IEnumerable<Piece> pieces)
+        {
+            this.ClearBoard();
+
+            foreach (var piece in pieces)
+            {
+                var cellImageName = this.GetCellName(piece.Position);
+                var image = this.BoardGrid.FindName(cellImageName) as Image;
+                if (image == null)
+                {
+                    Console.WriteLine("Image is invalid");
+                    continue;
+                }
+
+                image.Source = new BitmapImage(new Uri($"Images/{piece.PieceType.ToString()}{piece.Color.ToString()}.png", UriKind.Relative));
+            }
+        }
+
+        private void ClearBoard()
+        {
+            for (var y = 0; y < 8; y++)
+            {
+                for (var x = 0; x < 8; x++)
+                {
+                    var cellImageName = this.GetCellName(new Position(x, y));
+                    var image = this.BoardGrid.FindName(cellImageName) as Image;
+                    if (image == null)
+                    {
+                        Console.WriteLine("Image is invalid");
+                        continue;
+                    }
+
+                    image.Source = null;
+                }
+            }
+        }
+
         private void ChessCellClick(object sender, RoutedEventArgs e)
         {
             if (sender is Button button)
             {
                 var position = new Position(this.EvaluateLetter(button.Name[0]), 8 - (button.Name[1] - '0'));
             }
+        }
+
+        private string GetCellName(Position position)
+        {
+            var chessCharacters = new List<char> { 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H' };
+
+            return $"{chessCharacters[position.X]}{position.Y + 1}Image";
         }
 
         private int EvaluateLetter(char character)
